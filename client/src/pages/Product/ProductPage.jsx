3d92371d9987ba7fccc80b110b9aed56584d1191
Pcsrio <airyoldi@gmail.com>
2024-07-05 14:22:10 +0200

    template product page

--- client/src/pages/Product/ProductPage.jsx
+++ client/src/pages/Product/ProductPage.jsx
@@ -56,7 +56,7 @@ export default function ProductPage() {
             mousewheel
             keyboard
             modules={[Navigation, Pagination, Mousewheel, Keyboard]}
-            className="mySwiper"
+            className="swiperProduct"
           >
             <SwiperSlide>
               <img
