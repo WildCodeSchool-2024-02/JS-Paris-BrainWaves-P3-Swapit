eef5401eab2c7df45bf0d83825ab100e876a3d04
LUDOVIC SCELLES <ludovic.scelles@gmail.com>
2024-07-17 12:31:39 +0200

    US-09_Add_product_Creation_of_the_feature

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -224,103 +224,107 @@ export default function Profile() {
       </div>
       <div className="section-content">{renderSection()}</div>
 
-      {alignment === "Vitrine" && (
-        <div className="blocAddProduct">
-          <p className="addAProduct">Ajouter un produit</p>
+      {alignment === "Vitrine" &&
+        (auth.isLogged === true && (
+          <div className="blocAddProduct">
+            <p className="addAProduct">Ajouter un produit</p>
 
-          <div className="inputGroupAddProduct">
-            <p className="titleAddProduct">Titre&nbsp;:</p>
-            <input
-              type="text"
-              value={title}
-              onChange={handleTitleChange}
-              placeholder="Entrez le titre du produit"
-              className="inputTitleProd"
-            />
-          </div>
-
-          <div>
-            <p className="addAPhoto">Ajouter votre / vos photo(s) ici</p>
-            <div
-              className="photoPreview"
-              role="presentation"
-              onClick={triggerFileInput}
-            >
-              {productImage ? (
-                <img
-                  src={productImage}
-                  alt="product"
-                  style={{
-                    width: "50px",
-                    height: "50px",
-                    borderRadius: "50%",
-                  }}
-                />
-              ) : (
-                "+"
-              )}
+            <div className="inputGroupAddProduct">
+              <p className="titleAddProduct">Titre&nbsp;:</p>
+              <input
+                type="text"
+                value={title}
+                onChange={handleTitleChange}
+                placeholder="Entrez le titre du produit"
+                className="inputTitleProd"
+              />
             </div>
-            <input
-              id="fileInput"
-              type="file"
-              onChange={handleFileChange}
-              style={{ display: "none" }}
-            />
-          </div>
 
-          <p className="descriptionAddAProduct">Description&nbsp;:</p>
-          <textarea
-            type="text"
-            value={productDescription}
-            onChange={handleDescriptionChange}
-            className="texteAreaDescription"
-          />
+            <div>
+              <p className="addAPhoto">Ajouter votre / vos photo(s) ici</p>
+              <div
+                className="photoPreview"
+                role="presentation"
+                onClick={triggerFileInput}
+              >
+                {productImage ? (
+                  <img
+                    src={productImage}
+                    alt="product"
+                    style={{
+                      width: "50px",
+                      height: "50px",
+                      borderRadius: "50%",
+                    }}
+                  />
+                ) : (
+                  "+"
+                )}
+              </div>
+              <input
+                id="fileInput"
+                type="file"
+                onChange={handleFileChange}
+                style={{ display: "none" }}
+              />
+            </div>
 
-          <p className="conditionAddAProduct">Condition&nbsp;:</p>
-          <textarea
-            type="text"
-            value={productCondition}
-            onChange={handleConditionChange}
-            className="texteAreaCondition"
-          />
-
-          <div className="inputGroupAddProduct">
-            <p className="locationAddAProduct">Location&nbsp;:</p>
-            <input
+            <p className="descriptionAddAProduct">Description&nbsp;:</p>
+            <textarea
               type="text"
-              value={productLocation}
-              onChange={handleLocationChange}
-              className="inputLocation"
+              value={productDescription}
+              onChange={handleDescriptionChange}
+              className="texteAreaDescription"
             />
-          </div>
 
-          <div className="inputGroupAddProduct">
-            <p className="categoryAddAProduct">Catégorie&nbsp;:</p>
-            <select
-              value={selectedCategory}
-              onChange={handleCategoryChange}
-              className="inputCategory"
-            >
-              <option value="">Sélectionnez une catégorie</option>
-              {categories.map((category) => (
-                <option key={category.category_id} value={category.category_id}>
-                  {category.category_name}
-                </option>
-              ))}
-            </select>
-          </div>
+            <p className="conditionAddAProduct">Condition&nbsp;:</p>
+            <textarea
+              type="text"
+              value={productCondition}
+              onChange={handleConditionChange}
+              className="texteAreaCondition"
+            />
 
-          <div className="buttonZone">
-            <button
-              type="button"
-              className="validationAddAProduct"
-              onClick={handleSubmit}
-            >
-              Validation
-            </button>
+            <div className="inputGroupAddProduct">
+              <p className="locationAddAProduct">Location&nbsp;:</p>
+              <input
+                type="text"
+                value={productLocation}
+                onChange={handleLocationChange}
+                className="inputLocation"
+              />
+            </div>
+
+            <div className="inputGroupAddProduct">
+              <p className="categoryAddAProduct">Catégorie&nbsp;:</p>
+              <select
+                value={selectedCategory}
+                onChange={handleCategoryChange}
+                className="inputCategory"
+              >
+                <option value="">Sélectionnez une catégorie</option>
+                {categories.map((category) => (
+                  <option
+                    key={category.category_id}
+                    value={category.category_id}
+                  >
+                    {category.category_name}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div className="buttonZone">
+              <button
+                type="button"
+                className="validationAddAProduct"
+                onClick={handleSubmit}
+              >
+                Validation
+              </button>
+            </div>
           </div>
-        </div>
-      )}
+        ))}
     </>
   );
 }
