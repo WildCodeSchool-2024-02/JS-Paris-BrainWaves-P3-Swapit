ab861f1cc396a9d4238659184f9436e90927c298
Charlie Le <aizenjehuty@hotmail.fr>
2024-07-19 15:33:25 +0200

    update for pull

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -16,12 +16,9 @@ export default function Profile() {
 
   const today = new Date().toISOString().split("T")[0];
   const [productDate] = useState(today);
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, []);
-  const navigate = useNavigate();
+
   const [dataProduct, setDataProduct] = useState([]);
-  const [isModalOpen, setismodalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const [formData, setFormData] = useState({
     username: "",
     email: "",
@@ -32,18 +29,113 @@ export default function Profile() {
     postalCode: ""
   });
 
+  const { id } = useParams();
+  const [user, setUser] = useState([]);
+  const { auth, setAuth } = useOutletContext();
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/users/${id}`);
+        const data = await response.json();
+        setUser(data);
+        setFormData({
+          username: data.pseudo || "",
+          email: data.email || "",
+          password: "",
+          confirmPassword: "",
+          phone: data.phone || "",
+          city: data.city || "",
+          postalCode: data.postalCode || "",
+        });
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+      }
+    };
+
+    const fetchCategories = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/categories`);
+        const data = await response.json();
+        setCategories(data);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      }
+    };
+
+    const fetchUserItems = async () => {
+      try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/users/${id}/items`);
+        const data = await response.json();
+        setDataProduct(data);
+      } catch (error) {
+        console.error("Error fetching user items:", error);
+      }
+    };
+
+    fetchUserDetails();
+    fetchCategories();
+    fetchUserItems();
+  }, [id]);
+
+  useEffect(() => {
+    if (alignment === "Vitrine") {
+      fetch(`${import.meta.env.VITE_API_URL}/users/${id}/items`)
+        .then((response) => response.json())
+        .then((facts) => setDataProduct(facts));
+    }
+  }, [alignment, id]);
+
+  const handleProfileSubmit = async (e) => {
+    e.preventDefault();
+    if (formData.password !== formData.confirmPassword) {
+      toast.warning("Les mots de passe ne correspondent pas");
+      return;
+    }
+
+    const updatedUserData = {
+      pseudo: formData.username,
+      email: formData.email,
+      password: formData.password,
+      phone: formData.phone,
+      city: formData.city,
+      postalCode: formData.postalCode,
+    };
+
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/users/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${auth.token}`,
+        },
+        body: JSON.stringify(updatedUserData),
+      });
+
+      if (response.ok) {
+        const updatedUser = await response.json();
+        setUser(updatedUser);
+        toast.success("Profil mis à jour avec succès");
+        setIsModalOpen(false);
+      } else {
+        toast.warning("Échec de la mise à jour du profil");
+      }
+    } catch (error) {
+      toast.error("Une erreur est survenue lors de la mise à jour du profil");
+    }
+  };
+
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: value
     }));
-  };
-
-  const handleprofileSubmit = (e) => {
-    e.preventDefault();
-    // Handle form submission logic
-    setismodalOpen(false);
   };
 
   const handleRedirectionItem = (itemId) => {
@@ -55,19 +147,6 @@ export default function Profile() {
       setAlignment(newAlignment);
     }
   }, []);
-
-  const { id } = useParams();
-  const [user, setUser] = useState([]);
-
-  useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_URL}/users/${id}`)
-      .then((response) => response.json())
-      .then((data) => setUser(data));
-
-    fetch(`${import.meta.env.VITE_API_URL}/categories`)
-      .then((response) => response.json())
-      .then((data) => setCategories(data));
-  }, [id]);
 
   const handleTitleChange = (e) => {
     setTitle(e.target.value);
@@ -84,6 +163,7 @@ export default function Profile() {
   const triggerFileInput = () => {
     document.getElementById("fileInput").click();
   };
+
   const handleConditionChange = (e) => {
     setProductCondition(e.target.value);
   };
@@ -95,15 +175,34 @@ export default function Profile() {
   const handleCategoryChange = (e) => {
     setSelectedCategory(e.target.value);
   };
-  const rating = 1.3;
 
-  useEffect(() => {
-    if (alignment === "Vitrine") {
-      fetch(`${import.meta.env.VITE_API_URL}/users/${id}/items`)
-        .then((response) => response.json())
-        .then((facts) => setDataProduct(facts));
+  const handleSubmit = async () => {
+    const form = new FormData();
+    form.append("name", title);
+    form.append("description", productDescription);
+    form.append("conditions", productCondition);
+    form.append("date_added", productDate);
+    form.append("location", productLocation);
+    form.append(`image_url`, productImage);
+    form.append("user_id", id);
+    form.append("category_id", selectedCategory);
+
+    try {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/items`, {
+        method: "POST",
+        body: form,
+        headers: { Authorization: `Bearer ${auth.token}` },
+      });
+      if (response.ok) {
+        setAuth((prevState) => ({ ...prevState, user }));
+        toast.success("Vos modifications ont bien été prises en compte.");
+      } else {
+        toast.warning("Veuillez vérifier le format de vos données.");
+      }
+    } catch (error) {
+      toast.error("Une erreur est survenue . .");
     }
-  }, [alignment, id]);
+  };
 
   const renderSection = () => {
     switch (alignment) {
@@ -162,33 +261,7 @@ export default function Profile() {
     }
   };
 
-  const { auth, setAuth } = useOutletContext();
-
-  const handleSubmit = async () => {
-    const form = new FormData();
-    form.append("name", title);
-    form.append("description", productDescription);
-    form.append("conditions", productCondition);
-    form.append("date_added", productDate);
-    form.append("location", productLocation);
-    form.append(`image_url`, productImage);
-    form.append("user_id", id);
-    form.append("category_id", selectedCategory);
-
-    try {
-      const response = await fetch(`${import.meta.env.VITE_API_URL}/items`, {
-        method: "POST",
-        body: form,
-        headers: { Authorization: `Bearer ${auth.token}` },
-      });
-      if (response.ok) {
-        setAuth((prevState) => ({ ...prevState, user }));
-        toast.success("Vos modifications ont bien été prise en compte.");
-      } else toast.warning("Veuillez verifier le format de vos données.");
-    } catch (error) {
-      toast.error("Une erreur est survenue . .");
-    }
-  };
+  // const Rating = 1.3;
 
   return (
     <>
@@ -200,35 +273,37 @@ export default function Profile() {
           <div className="profileDetails">
             <h2>{user.pseudo}</h2>
             <div className="Five-Rate-Active Larger">
-              <p className="screenReaders">Rated {rating} out of 5</p>
+              <p className="screenReaders">Rated {user.rating} out of 5</p>
               {[1, 2, 3, 4, 5].map((rate) => (
                 <button
                   id="buttonStar"
                   key={rate}
                   type="button"
                   className={
-                    rate <= rating ? "rate-value-full" : "rate-value-empty"
+                    rate <= user.rating ? "rate-value-full" : "rate-value-empty"
                   }
                   aria-label={`Rate ${rate} out of 5`}
                 >
-                  <span aria-hidden="true" />
+                  <span aria-hidden="true">
+                    <img src="./star.svg" alt="star" />
+                  </span>
                 </button>
               ))}
             </div>
             <p className="Location">Paris, France</p>
             <p className="Subscribe">Member since January 2024</p>
             <div>
-              <button id="buttonModif" type="button" onClick={() =>setismodalOpen(true)}>
+              <button id="buttonModif" type="button" onClick={() => setIsModalOpen(true)}>
                 Modifications profil
               </button>
               {isModalOpen && (
                 <div className="modalProfile">
                   <div className="modalcontentProfile">
-                    <span className="closeBtn" role="presentation" onClick={() => setismodalOpen(false)}>&times;</span>
-                    <form onSubmit={handleprofileSubmit} id="profileForm">
-                    <input
+                    <span className="closeBtn" role="presentation" onClick={() => setIsModalOpen(false)}>&times;</span>
+                    <form onSubmit={handleProfileSubmit} id="profileForm">
+                      <input
                         placeholder="Pseudo"
-                        type="Pseudo"
+                        type="text"
                         id="username"
                         name="username"
                         value={formData.username}
@@ -246,7 +321,7 @@ export default function Profile() {
                       />
                       <input 
                         placeholder="Mot de passe"
-                        type="Mot de passe"
+                        type="password"
                         id="password"
                         name="password"
                         value={formData.password}
@@ -273,7 +348,7 @@ export default function Profile() {
                       />
                       <input
                         placeholder="Ville"
-                        type="ville"
+                        type="text"
                         id="city"
                         name="city"
                         value={formData.city}
@@ -282,7 +357,7 @@ export default function Profile() {
                       />
                       <input
                         placeholder="Code Postal"
-                        type="Code Postal"
+                        type="text"
                         id="postalCode"
                         name="postalCode"
                         value={formData.postalCode}
@@ -290,8 +365,8 @@ export default function Profile() {
                         required
                       />
                       <div id="confirmationprofileBtn">
-                      <button type="submit" className="confirmationBtn">Confirmation</button>
-                    </div>
+                        <button type="submit" className="confirmationBtn">Confirmation</button>
+                      </div>
                     </form>
                   </div>
                 </div>
@@ -321,107 +396,106 @@ export default function Profile() {
       </div>
       <div className="section-content">{renderSection()}</div>
 
-      {alignment === "Vitrine" &&
-        (auth.isLogged === true && (
-          <div className="blocAddProduct">
-            <p className="addAProduct">Ajouter un produit</p>
+      {alignment === "Vitrine" && auth.isLogged && (
+        <div className="blocAddProduct">
+          <p className="addAProduct">Ajouter un produit</p>
 
-            <div className="inputGroupAddProduct">
-              <p className="titleAddProduct">Titre&nbsp;:</p>
-              <input
-                type="text"
-                value={title}
-                onChange={handleTitleChange}
-                placeholder="Entrez le titre du produit"
-                className="inputTitleProd"
-              />
-            </div>
-
-            <div>
-              <p className="addAPhoto">Ajouter votre / vos photo(s) ici</p>
-              <div
-                className="photoPreview"
-                role="presentation"
-                onClick={triggerFileInput}
-              >
-                {productImage ? (
-                  <img
-                    src={productImage}
-                    alt="product"
-                    style={{
-                      width: "50px",
-                      height: "50px",
-                      borderRadius: "50%",
-                    }}
-                  />
-                ) : (
-                  "+"
-                )}
-              </div>
-              <input
-                id="fileInput"
-                type="file"
-                onChange={handleFileChange}
-                style={{ display: "none" }}
-              />
-            </div>
-
-            <p className="descriptionAddAProduct">Description&nbsp;:</p>
-            <textarea
+          <div className="inputGroupAddProduct">
+            <p className="titleAddProduct">Titre&nbsp;:</p>
+            <input
               type="text"
-              value={productDescription}
-              onChange={handleDescriptionChange}
-              className="texteAreaDescription"
+              value={title}
+              onChange={handleTitleChange}
+              placeholder="Entrez le titre du produit"
+              className="inputTitleProd"
             />
-
-            <p className="conditionAddAProduct">Condition&nbsp;:</p>
-            <textarea
-              type="text"
-              value={productCondition}
-              onChange={handleConditionChange}
-              className="texteAreaCondition"
-            />
-
-            <div className="inputGroupAddProduct">
-              <p className="locationAddAProduct">Location&nbsp;:</p>
-              <input
-                type="text"
-                value={productLocation}
-                onChange={handleLocationChange}
-                className="inputLocation"
-              />
-            </div>
-
-            <div className="inputGroupAddProduct">
-              <p className="categoryAddAProduct">Catégorie&nbsp;:</p>
-              <select
-                value={selectedCategory}
-                onChange={handleCategoryChange}
-                className="inputCategory"
-              >
-                <option value="">Sélectionnez une catégorie</option>
-                {categories.map((category) => (
-                  <option
-                    key={category.category_id}
-                    value={category.category_id}
-                  >
-                    {category.category_name}
-                  </option>
-                ))}
-              </select>
-            </div>
-
-            <div className="buttonZone">
-              <button
-                type="button"
-                className="validationAddAProduct"
-                onClick={handleSubmit}
-              >
-                Validation
-              </button>
-            </div>
           </div>
-        ))}
+
+          <div>
+            <p className="addAPhoto">Ajouter votre / vos photo(s) ici</p>
+            <div
+              className="photoPreview"
+              role="presentation"
+              onClick={triggerFileInput}
+            >
+              {productImage ? (
+                <img
+                  src={URL.createObjectURL(productImage)}
+                  alt="product"
+                  style={{
+                    width: "50px",
+                    height: "50px",
+                    borderRadius: "50%",
+                  }}
+                />
+              ) : (
+                "+"
+              )}
+            </div>
+            <input
+              id="fileInput"
+              type="file"
+              onChange={handleFileChange}
+              style={{ display: "none" }}
+            />
+          </div>
+
+          <p className="descriptionAddAProduct">Description&nbsp;:</p>
+          <textarea
+            type="text"
+            value={productDescription}
+            onChange={handleDescriptionChange}
+            className="texteAreaDescription"
+          />
+
+          <p className="conditionAddAProduct">Condition&nbsp;:</p>
+          <textarea
+            type="text"
+            value={productCondition}
+            onChange={handleConditionChange}
+            className="texteAreaCondition"
+          />
+
+          <div className="inputGroupAddProduct">
+            <p className="locationAddAProduct">Location&nbsp;:</p>
+            <input
+              type="text"
+              value={productLocation}
+              onChange={handleLocationChange}
+              className="inputLocation"
+            />
+          </div>
+
+          <div className="inputGroupAddProduct">
+            <p className="categoryAddAProduct">Catégorie&nbsp;:</p>
+            <select
+              value={selectedCategory}
+              onChange={handleCategoryChange}
+              className="inputCategory"
+            >
+              <option value="">Sélectionnez une catégorie</option>
+              {categories.map((category) => (
+                <option
+                  key={category.category_id}
+                  value={category.category_id}
+                >
+                  {category.category_name}
+                </option>
+              ))}
+            </select>
+          </div>
+
+          <div className="buttonZone">
+            <button
+              type="button"
+              className="validationAddAProduct"
+              onClick={handleSubmit}
+            >
+              Validation
+            </button>
+          </div>
+        </div>
+      )}
     </>
   );
 }
